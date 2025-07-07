907438f65ee38c468dcd6d48f18df5a7b16afd9e
Cícero Mello <ciceromello.dev@gmail.com>
2025-07-07 14:48:58 -0300

    ✨ Hello World

---
+++ hello-world/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+// para compilar: rustc main.rs (já estando na pasta "hello-world")
+/*
+    Isso vai gerar um arquivo binario "main" (sem extensão).
+*/
+
+// para executar o build: ./main
+
+/*
+    Obs: RUST possui baixa sinergia com Windows, portanto,
+    instalei uma distro Linux (Ubuntu) e configurei a linguagem
+    na máquina pelo WSL. Ou seja, todos os comandos falados
+    consideram que o projeto está em Linux (não Windows).
+*/
